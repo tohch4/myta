419a650a5056bbe9891460fb3573edc3371cdbda
Alexander Stein <astein@flexion.us>
2018-09-19 01:06:45 -0400

    [app] First PoC Hello World like example.

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ This is a proof of concept tool to programmatically gather, collate. and
 analyze TrustedAdvisor output for 1 or more AWS accounts. Filtering or template
 logic can be applied to analyze or react to only the desired subset.
 
+# Quickstart
+
+Use traditional AWS_PROFILE environment variable and give it a whirl.
+
+```
+export AWS_PROFILE=My_AWS_Profile_Name # Check ~/.aws/credentials if unsure
+mvn exec:java -Dexec.mainClass="gov.cms.qpp.fc.myta.App"
+```
+
 # Prerequisites
 
 ## Development
--- src/main/java/gov/cms/qpp/fc/myta/App.java
+++ src/main/java/gov/cms/qpp/fc/myta/App.java
@@ -1,13 +1,25 @@
 package gov.cms.qpp.fc.myta;
+import com.amazonaws.services.support.*;
+import com.amazonaws.services.support.model.*;
+public class App {
+    private static AWSSupportClient createClient() {
+        AWSSupportClient client = new AWSSupportClient();
+        client.setEndpoint("https://support.us-east-1.amazonaws.com");
+        return client;
+    }
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void getTaChecks(AWSSupportClient client) {
+        DescribeTrustedAdvisorChecksResult result = client
+                .describeTrustedAdvisorChecks(new DescribeTrustedAdvisorChecksRequest().withLanguage("en"));
+
+        for (TrustedAdvisorCheckDescription checkDescription : result.getChecks()) {
+            System.out.println(checkDescription.getId());
+            System.out.println(checkDescription.getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        AWSSupportClient client = createClient();
+        getTaChecks(client);
     }
 }
